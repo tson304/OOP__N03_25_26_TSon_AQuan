c1423ecaaef0b65109a054a6e989d8d02f225d59
tson304 <149238613+tson304@users.noreply.github.com>
2025-09-12 18:02:38 +0700

    Pratice 4 - TranDucThaiSon_23017229

---
+++ QuanLyNhac/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Test thư viện nhạc (Trần Đức Thái Sơn - 23017229)
+        testThuVienNhac test = new testThuVienNhac();
+        test.thuVien();
+    }
+}
